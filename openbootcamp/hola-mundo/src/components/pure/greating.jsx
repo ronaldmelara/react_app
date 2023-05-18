66676b02b6bbc33831d2c1144a6499a626f1c654
ronaldmelara <ronaldmelara@gmail.com>
2023-05-18 19:49:33 -0400

    proy HolaMundo OpenBootcamp

---
+++ openbootcamp/hola-mundo/src/components/pure/greating.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Greating extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Greating</h1>
+            </div>
+        );
+    }
+}
+
+Greating.propTypes = {
+
+};
+
+export default Greating;
